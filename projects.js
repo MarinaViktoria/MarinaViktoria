3f6d4514a26a3939bf76a6e74e7084fc7cd264cf
Misushka <maryna.seidel@yahoo.de>
2024-06-18 11:05:17 +0200

    first commit

---
+++ projects.js
@@ -0,0 +1,100 @@
+const imageOne = document.querySelector(".imageOne");
+btn.addEventListener("click", openOne);
+function openOne(){
+    window.open("https://stormy-pretty-dish.glitch.me/")
+}
+
+const imageTwo = document.querySelector(".imageTwo");
+btn.addEventListener("click", openTwo);
+function openTwo(){
+    window.open("https://chic-bonbon-4619dd.netlify.app")
+}
+
+const imageThree = document.querySelector(".imageThree");
+btn.addEventListener("click", openThree);
+function openThree(){
+    window.open("https://admirable-stroopwafel-6e5f3f.netlify.app")
+}
+
+const imageFour = document.querySelector(".imageFour");
+imageFour.addEventListener("click", openFour);
+function openFour(){
+    window.open("https://maroon-fog-marionberry.glitch.me/")
+}
+
+const imageFive = document.querySelector(".imageFive");
+imageFive.addEventListener("click", openFive);
+function openFive(){
+    window.open("https://rhetorical-buttered-brake.glitch.me/")
+}
+
+const imageSix = document.querySelector(".imageSix");
+imageSix.addEventListener("click", openSix);
+function openSix(){
+    window.open("https://charming-orange-mule.glitch.me/")
+}
+
+const imageSeven = document.querySelector(".imageSeven");
+imageSeven.addEventListener("click", openSeven);
+function openSeven(){
+    window.open("https://emerald-bevel-lifter.glitch.me/")
+}
+
+const imageEight = document.querySelector(".imageEight");
+imageEight.addEventListener("click", openEight);
+function openEight(){
+    window.open("https://humane-zircon-flock.glitch.me/")
+}
+
+const imageNine = document.querySelector(".imageNine");
+imageNine.addEventListener("click", openNine);
+function openNine(){
+    window.open("https://mewing-pewter-mango.glitch.me/")
+}
+
+const imageTen = document.querySelector(".imageTen")
+imageTen.addEventListener("click", openTen);
+function openTen(){
+    window.open("https://sable-lucky-exception.glitch.me/")
+}
+
+const imageEleven = document.querySelector(".imageEleven");
+imageEleven.addEventListener("click", openEleven);
+function openEleven(){
+    window.open("https://expensive-awake-roadrunner.glitch.me/")
+}
+
+const imageTwelve = document.querySelector(".imageTwelve");
+imageTwelve.addEventListener("click", openTwelve);
+function openTwelve(){
+    window.open("https://shade-juicy-allosaurus.glitch.me/")
+}
+
+const imageThirteen = document.querySelector(".imageThirteen");
+imageThirteen.addEventListener("click", openThirteen);
+function openThirteen(){
+    window.open("https://far-ivy-pudding.glitch.me/")
+}
+
+const imageFourteen = document.querySelector(".imageFourteen");
+imageFourteen.addEventListener("click", openFourteen);
+function openFourteen(){
+    window.open("https://venerable-semolina-b8c711.netlify.app/")
+}
+const imageFifteen = document.querySelector(".imageFifteen");
+imageFifteen.addEventListener("click", openFifteen);
+function openFifteen(){
+    window.open("https://silver-seahorse-468c18.netlify.app/")
+}
+const imageSixteen = document.querySelector(".imageSixteen");
+imageSixteen.addEventListener("click", openSixteen);
+function openSixteen(){
+    window.open("https://my-very-new-todo-list.netlify.app/")
+}
+const imageSeventeen = document.querySelector(".imageSeventeen");
+imageSeventeen.addEventListener("click", openSeventeen);
+function openSeventeen(){
+    window.open("https://my-new-fashion-store.netlify.app/")
+}
+
+
